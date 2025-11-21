8906b6b2ba8e4b6e02bbd11083b03b65039dc8d8
Piotr Orczykowski <orczykowski.peter@gmail.com>
2025-11-21 07:57:46 +0100

    Add config implementation

---
+++ app/core/config.py
@@ -0,0 +1,10 @@
+from functools import lru_cache
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+
+@lru_cache
+def get_settings():
+    return Settings()
