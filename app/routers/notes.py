362af3b0b0cc3baf6e40e572e286337c47233e31
Piotr Orczykowski <orczykowski.peter@gmail.com>
2025-12-29 15:31:07 +0100

    Feat/note management (#2)
    * Add note implementation PART 1
    
    * Add note implementation PART 2
    
    * Fix types

---
+++ app/routers/notes.py
@@ -0,0 +1,63 @@
+from fastapi import APIRouter, HTTPException, status
+from fastapi.params import Depends
+from sqlmodel import Session
+from app.dependencies import get_db
+from app.models.note import Note
+from app.schemas.error import ErrorResponse
+from app.schemas.note import NoteQuery, NoteUpsert, NoteList, NoteResponse
+from app.services.note_service import (
+    create_note,
+    delete_note,
+    get_all_notes,
+    update_note,
+)
+
+
+router = APIRouter(prefix="/notes", tags=["Note"])
+
+
+@router.get(
+    "",
+    summary="Get all notes",
+    response_description="List of notes",
+    response_model=NoteList,
+)
+def get_all(query: NoteQuery = Depends(), db: Session = Depends(get_db)):
+    return get_all_notes(db, query)
+
+
+@router.post(
+    "",
+    summary="Create a new note",
+    response_description="The created note",
+    status_code=status.HTTP_201_CREATED,
+    response_model=NoteResponse,
+)
+def create(note: NoteUpsert, db: Session = Depends(get_db)) -> Note:
+    return create_note(db, note)
+
+
+@router.put(
+    "/{note_id}",
+    summary="Update a note",
+    responses={
+        200: {"model": NoteResponse, "description": "Note updated successfully"},
+        404: {"model": ErrorResponse, "description": "Note not found"},
+    },
+    response_model=NoteResponse,
+)
+def update(note_id: int, note: NoteUpsert, db: Session = Depends(get_db)) -> Note:
+    updated_note = update_note(db, note_id, note)
+    if not updated_note:
+        raise HTTPException(status_code=404, detail="Note not found")
+    return updated_note
+
+
+@router.delete(
+    "/{note_id}",
+    summary="Delete a note",
+    response_description="Deletion status",
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+def delete(note_id: int, db: Session = Depends(get_db)) -> None:
+    delete_note(db, note_id)
---
+++ app/schemas/note.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+from app.schemas.paging import PagingMetadata
+
+
+class NoteQuery(BaseModel):
+    q: Optional[str] = None
+    page: int = Field(1, ge=1)
+    page_size: int = Field(10, ge=1, le=100)
+
+
+class NoteUpsert(BaseModel):
+    title: Optional[str] = Field(None, max_length=100)
+    content: str = Field(
+        ...,
+        min_length=1,
+        max_length=1000,
+    )
+
+
+class NoteResponse(BaseModel):
+    id: int
+    title: Optional[str]
+    content: str
+
+
+class NoteList(BaseModel):
+    data: List[NoteResponse]
+    meta: PagingMetadata
