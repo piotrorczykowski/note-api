d07f6eb99c94f1bd16b5420f68e78b2c6a5af3f1
Piotr Orczykowski <orczykowski.peter@gmail.com>
2025-12-31 10:31:37 +0100

    Add validation

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -1,15 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
 
 
 class UserSingIn(BaseModel):
-    email: str
-    password: str
+    email: EmailStr
+    password: str = Field(..., min_length=6, max_length=64)
 
 
 class UserSingUp(BaseModel):
-    email: str
-    full_name: str
-    password: str
+    email: EmailStr
+    full_name: str = Field(..., min_length=2, max_length=50)
+    password: str = Field(..., min_length=6, max_length=64)
 
 
 class UserResponse(BaseModel):
